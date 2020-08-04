bdd0b67699d59ac8a464fe84bfc6b05eb295a3f8
haleykelling <haley@kellings.com>
2020-08-04 12:00:31 -0600

    Create src folder with screens and components folders. Create home screen with text element. Create trip component with text element.

---
+++ src/components/Trip.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { Text, View, StyleSheet } from 'react-native';
+
+const Trip = () => {
+    return (
+        <View>
+            <Text>This is a Trip Component</Text>
+        </View>
+    );
+}
+
+const styles = StyleSheet.create({
+    
+})
+
+export default Trip;
