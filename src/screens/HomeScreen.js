c8afb4933acc38196af7992871a1e452de4c5b9f
haleykelling <haley@kellings.com>
2020-08-08 10:02:48 -0600

    Add custom fonts and styling to home screen.

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,31 +1,53 @@
 import React from 'react';
-import { Text, View, Button, ImageBackground, StyleSheet } from 'react-native';
+import { Text, View, TouchableOpacity, ImageBackground, StyleSheet } from 'react-native';
+import { useFonts, DancingScript_700Bold } from '@expo-google-fonts/dancing-script';
+import { PlayfairDisplay_400Regular } from '@expo-google-fonts/playfair-display';
+import { AppLoading } from 'expo'
 
 const HomeScreen = ({navigation}) => {
-    return (
+    let [fontsLoaded] = useFonts({
+        DancingScript_700Bold,
+        PlayfairDisplay_400Regular
+    });
+    
+    if (!fontsLoaded){
+        return <AppLoading />
+    } else {
+        return (
         <View style={styles.viewStyle}>
             <ImageBackground source={require('../../assets/boat_lake.jpg')} style={styles.backgroundStyle} >
-                <Text style={styles.textStyle}>Home Screen</Text>
-                <Button title="See My Trips" onPress={() => navigation.navigate('Trip')} />
+                <Text style={[styles.headingStyle, { fontFamily: 'DancingScript_700Bold' }]}>Wanderlust</Text>
+                <TouchableOpacity onPress={() => navigation.navigate('Trip')} >
+                    <Text style={styles.textStyle}>plan your next adventure</Text>
+                </TouchableOpacity>
             </ImageBackground>
         </View>
-    );
+    )}
 }
 
 const styles = StyleSheet.create({
     viewStyle: {
         flex: 1,
-        alignItems: 'center',
-        justifyContent: 'center',
     },
     backgroundStyle: {
         flex: 1,
         resizeMode: 'cover',
-        width: 400,
-        height: 400
+        width: 420,
+        height: 1000,
+        justifyContent: 'flex-start'
+    },
+    headingStyle: {
+        fontSize: 75,
+        color: 'hsl(215, 90%, 20%)',
+        marginTop: 20,
+        marginBottom: 5,
+        marginLeft: 20
     },
     textStyle: {
-        fontSize: 26
+        marginLeft: 35,      
+        fontSize: 22,
+        color: 'hsl(215, 90%, 20%)',
+        fontFamily: 'PlayfairDisplay_400Regular'
     }
 })
 
