e614d94423f723bc9123acfb2992d3592053ef2d
haleykelling <haley@kellings.com>
2020-08-10 11:56:39 -0600

    Fetch call added to Itinerary Screen to get days and activities.

--- src/screens/DayScreen.js
+++ src/screens/DayScreen.js
@@ -1,13 +1,12 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import { Text, ScrollView, StyleSheet, FlatList, TouchableOpacity } from 'react-native';
 import Event from '../components/Event';
 import Map from '../components/Map';
 
-const DayScreen = ({route, navigation}) => {
-    const {day} = route.params
+const DayScreen = ({navigation}) => {
+
     const activities = day.activities
     const transportations = day.transportations
-    const restaurants = day.restaurants
     const allEvents = activities.concat(transportations.concat(restaurants))
     const allEventsSorted = allEvents.sort_by((a, b) => a.start_time.to_i - b.start_time.to_i)
     
--- src/screens/ItineraryScreen.js
+++ src/screens/ItineraryScreen.js
@@ -1,23 +1,49 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import { Text, ScrollView, StyleSheet, FlatList, TouchableOpacity } from 'react-native';
 import Day from '../components/Day';
 import Map from '../components/Map';
+import { AppLoading } from 'expo';
+
+
+const daysUrl = 'http://localhost:3000/days'
 
 const ItineraryScreen = ({route, navigation}) => {
     const {trip} = route.params
-    const sortedDays = trip.days.sort((a,b) => {
+
+    const [days, setDays] = useState([])
+    
+    const sortedDays = () => days.sort((a,b) => {
         return new Date(a.date) - new Date(b.date)
     })
-    console.log(trip)
+
+    useEffect(() => {
+        fetch(daysUrl, {
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({trip_id: trip.id})
+        })
+            .then(response => response.json())
+            .then(result => {
+                setDays(result)
+                setDays(sortedDays())
+            })
+    }, [])
     
-    return (
-        <FlatList 
-            data={sortedDays}
-            keyExtractor={(day) => day.id.toString()}
-            renderItem={({item, index}) => <Day day={item} index={index} navigation={navigation}/>}
-            ListHeaderComponent={<Map />}
-        />
-    );
+    if (days.length === 0){
+        return <AppLoading />
+    } else {
+        return (
+            <FlatList 
+                data={days}
+                keyExtractor={(day) => day.id.toString()}
+                renderItem={({item, index}) => <Day day={item} index={index} navigation={navigation}/>}
+                ListHeaderComponent={<Map />}
+            />
+        );
+    }
+    
 }
 
 export default ItineraryScreen;
